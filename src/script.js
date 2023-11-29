507c602a69b9ecb0de0a2f014f704a8cdfe23378
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-29 20:53:10 +0100

    lesson-9-1

--- src/script.js
+++ src/script.js
@@ -3,7 +3,12 @@
 // https://bool.dev/blog/detail/obyasnenie-event-loop-v-javascript-s-pomoshchyu-vizualizatsii
 // https://dev.to/lydiahallie/javascript-visualized-event-loop-3dif
 
-
+for (let i = 3; i > 0; i-- ){
+    // console.log(i);
+    const delay = i * 1000;
+    console.log(delay);
+    // setTimeout(() => console.log(i), delay);
+}
 
 // Приклад використання setInterval та setTimeout
 // Створимо блок з рекламою яка зникне через 10 секунд

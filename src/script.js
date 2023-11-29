db23045fd88c74796c21dcf114d855e533c1f298
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-29 14:44:46 +0100

    src-base

---
+++ src/script.js
@@ -0,0 +1,9 @@
+// Додатковий матеріал для самостійного ознайомлення
+// Event loop documentation
+// https://bool.dev/blog/detail/obyasnenie-event-loop-v-javascript-s-pomoshchyu-vizualizatsii
+// https://dev.to/lydiahallie/javascript-visualized-event-loop-3dif
+
+
+
+// Приклад використання setInterval та setTimeout
+// Створимо блок з рекламою яка зникне через 10 секунд

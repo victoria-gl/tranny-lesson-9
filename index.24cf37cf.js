54504017831fcc73caa3250cb7f4e6ce3a81cf49
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-30 15:36:49 +0000

    Deploying to gh-pages from @ victoria-gl/tranny-lesson-9@ff14b76c598429a925095532e6e6b228eb869ec7 🚀

--- index.24cf37cf.js
+++
@@ -1,2 +0,0 @@
-var promise=new Promise((function(e,o){var n=2e3*Math.random();setTimeout((function(){n>1e3?e("Drink beer"):o("Go home")}),2e3)}));console.log(promise);
-//# sourceMappingURL=index.24cf37cf.js.map
